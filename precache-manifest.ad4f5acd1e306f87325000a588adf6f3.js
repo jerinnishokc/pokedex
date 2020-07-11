91ca4ebad5892276df01b943844726c453851f5a
jerinnishokc <jerinnishok@outlook.com>
2020-07-11 18:32:20 +0530

    Updates

---
+++ precache-manifest.ad4f5acd1e306f87325000a588adf6f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7d5b3ed3e1a39caa73222eccb2bb2c",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "7873798d4ff4c34c069f",
+    "url": "/pokedex/static/css/main.b89c9cdd.chunk.css"
+  },
+  {
+    "revision": "bf5d2f8250f89afad92c",
+    "url": "/pokedex/static/js/2.7134a70e.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/pokedex/static/js/2.7134a70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7873798d4ff4c34c069f",
+    "url": "/pokedex/static/js/main.b390bd35.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  },
+  {
+    "revision": "83d6b10f76a5d00417888afdf053ad1a",
+    "url": "/pokedex/static/media/pokeball.83d6b10f.png"
+  }
+]);
